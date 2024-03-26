160ee57e676f7db4177772c2da2105c5f8ca5dac
vikassah03 <88377512+vikassah03@users.noreply.github.com>
2024-03-26 14:29:41 +0530

    Add files via upload

---
+++ customslider/jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#tslider").owlCarousel({
+      items: 4,
+      itemsDesktop: [1000, 2],
+      itemsDesktopSmall: [980, 1],
+      itemsTablet: [768, 1],
+      // pagination: true,
+      // navigation: true,
+      // navigationText: ["<", ">"],
+      autoPlay: true
+    });
+  });
+  
